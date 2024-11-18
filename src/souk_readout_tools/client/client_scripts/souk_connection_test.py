68d97f5bee26c8e636eb3b2467422bd2deedb682
Sam Rowe <sam.rowe@astro.cf.ac.uk>
2024-11-18 12:22:52 +0000

    souk-connection-test now correctly takes config_file as option

--- src/souk_readout_tools/client/client_scripts/souk_connection_test.py
+++ src/souk_readout_tools/client/client_scripts/souk_connection_test.py
@@ -1,12 +1,26 @@
 from .. import readout_client
 from .. import bcolors
 
+import argparse
+import os
+
 def main():
     try:
-        client = readout_client.ReadoutClient()
-        info = client.get_server_status()
-        if info['status']=='success':
+        DEFAULT_CONFIG = os.path.expanduser('~/.souk_readout_tools/config/default_config.lnk')
+
+        parser = argparse.ArgumentParser(description='Check if the souk_readout_tools server is running on RFSoC')
+        parser.add_argument('-C', '--config_file', type=str,
+                            default=DEFAULT_CONFIG,
+                            help='Path to the configuration file, Reverts to ~/souk_readout_tools/config/default.lnk if not given.')
+
+        args = parser.parse_args()
+
+        client = readout_client.ReadoutClient(args.config_file)
+        result = client.get_server_status()
+        
+        if result['status']=='success':
             print(bcolors.OKGREEN+'Success'+bcolors.ENDC)
+            print(f"SOUK Readout Server running on {result['message']['ip_addresses']}")
         else:
             print(bcolors.FAIL+'Failed to get server status:\n'+bcolors.ENDC,info)
     except Exception as e:
