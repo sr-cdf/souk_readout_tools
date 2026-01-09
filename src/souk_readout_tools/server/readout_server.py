24707b3174a06fdb8a7f3881de5b552ca244f9c5
Sam Rowe <sam.rowe@astro.cf.ac.uk>
2026-01-09 15:46:09 +0000

    dont raise in firmware init_server if firmware not supported

--- src/souk_readout_tools/server/readout_server.py
+++ src/souk_readout_tools/server/readout_server.py
@@ -41,8 +41,20 @@ import base64
 from souk_readout_tools import calibration
 from souk_readout_tools import firmware_lib
 
-
 import time
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 #STREAM FLAGS 
 FLAG_SERVER_REQUEST = 0
@@ -320,8 +332,8 @@ class ReadoutServer:
         try:
             system_information = self.get_system_information()
         except Exception as e:
-            print('Warning: could not get system information from firmware:',e)
-            raise e
+            print(bcolors.WARNING+'Warning: could not get system information from firmware:',e,bcolors.ENDC)
+            print('Try hard reset')
         return
    
              
